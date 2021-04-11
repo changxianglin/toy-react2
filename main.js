ae76742b8f025fd1c2d409d37c418022a126cff8
changxianglin <281532612@qq.com>
2021-04-11 12:54:38 +0800

    auto update data

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ class MyComponent extends Component {
   render() {
     return <div>
       <h1>my component</h1>
-      <button onclick={() => {this.state.a++; this.rerender()}}>add</button>
+      <button onclick={() => { this.setState({a: this.state.a + 1 }) }}>add</button>
       <span>{this.state.a.toString()}</span>
+      <span>{this.state.b.toString()}</span>
     </div>
   }
 }
